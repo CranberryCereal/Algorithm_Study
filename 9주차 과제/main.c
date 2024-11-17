fe24993d7c57fb818cbfeb86fb515f92c3af8f75
CranberryCereal <jcid0731@naver.com>
2024-11-17 14:59:20 +0900

    큐 기반 시뮬레이션 과제입니다.
    주어진 QueueType을 이용하여 일반고객 큐와 Vip 큐를 분리하였고, 이 두개의 큐를 이용하여 우선순위에 따라 업무를 처리하도록 구성하였습니다.

---
+++ 9주차 과제/main.c
@@ -0,0 +1,142 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#define MAX_QUEUE_SIZE 5
+typedef struct { // 요소 타입
+	int id;
+	int arrival_time;
+	int service_time;
+} element;
+typedef struct { // 원형큐 타입
+	element  data[MAX_QUEUE_SIZE];
+	int front, rear;
+} QueueType;
+
+
+
+void init_queue(QueueType* q)
+{
+	q->front = q->rear = 0;
+}
+
+
+int is_empty(QueueType* q)
+{
+	return (q->front == q->rear);
+}
+
+
+int is_full(QueueType* q)
+{
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+
+void enqueue(QueueType* q, element item)
+{
+	if (is_full(q))
+		printf("대기열이 가득 차 있습니다. 고객이 돌아갑니다.\n");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType* q)
+{
+	if (is_empty(q))
+		printf("대기열에 아무도 없습니다.\n");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+int main(void)
+{
+	int minutes = 50000;
+	
+	int total_wait = 0;
+	int total_customers = 0;
+	int service_time = 0;
+	int service_customer;
+
+	int vip_wait = 0;
+	int vip_customers = 0;
+	int regular_wait = 0;
+	int regular_customers = 0;
+
+	int randValue = 0;
+
+	QueueType regular;
+	QueueType Vip;
+	init_queue(&regular);
+	init_queue(&Vip);
+	srand(time(NULL));
+
+	for (int clock = 0; clock < minutes; clock++) {
+		printf("현재시각=%d\n", clock);
+		randValue = rand() % 10;
+
+		if (randValue < 3) { // 전체 고객 발생 확률: 30%
+
+			element customer;
+			customer.id = total_customers++;
+			customer.arrival_time = clock;
+			customer.service_time = rand() % 3 + 1;  // 고객 업무 소요 시간
+			
+
+			if (randValue < 1) { // Vip 고객 발생 확률 : 10%, 나머지 20%는 일반고객 발생
+				enqueue(&Vip, customer);
+				vip_customers++;
+				printf("VIP 고객 %d이 %d분에 들어옵니다. 업무처리시간= %d분\n",
+					customer.id, customer.arrival_time, customer.service_time);
+			}
+
+			else { // 일반 고객 발생 확률 : 20%
+				enqueue(&regular, customer);
+				regular_customers++;
+				printf("일반 고객 %d이 %d분에 들어옵니다. 업무처리시간= %d분\n",
+					customer.id, customer.arrival_time, customer.service_time);
+			}
+		}
+
+		if (service_time > 0) {
+			printf("고객 %d 업무처리중입니다. \n", service_customer);
+			service_time--;
+		}
+
+		else {
+			if (!is_empty(&Vip)) { //Vip 큐에 대기인원이 있다면 먼저 처리
+				element customer = dequeue(&Vip);
+				service_customer = customer.id;
+				service_time = customer.service_time;
+				printf("VIP고객 %d이 %d분에 업무 시작, 대기시간은 %d분.\n",
+					customer.id, clock, clock - customer.arrival_time);
+				total_wait += clock - customer.arrival_time;
+				vip_wait += clock - customer.arrival_time;
+			}
+			else if (!is_empty(&regular)) {//Vip 큐에 대기인원이 없으며 일반고객 큐에 대기인원이 있다면 처리
+				element customer = dequeue(&regular);
+				service_customer = customer.id;
+				service_time = customer.service_time;
+				printf("일반고객 %d이 %d분에 업무 시작, 대기시간은 %d분.\n",
+					customer.id, clock, clock - customer.arrival_time);
+				total_wait += clock - customer.arrival_time;
+				regular_wait += clock - customer.arrival_time;
+			}
+		}
+	}
+
+	printf("\n");
+	printf("VIP 고객 전체 대기 시간 = %d분 \n", vip_wait);
+	printf("VIP 고객 방문 수 = %d명 \n",vip_customers);
+	printf("VIP 고객 평균 대기 시간 = %d분 \n", vip_wait / vip_customers);
+	printf("\n");
+	printf("일반고객 전체 대기 시간 = %d분 \n", regular_wait);
+	printf("일반고객 방문 수 = %d명 \n", regular_customers);
+	printf("일반고객 평균 대기 시간 = %d분 \n", regular_wait / regular_customers);
+	printf("\n");
+	printf("전체고객 전체 대기 시간 = %d분 \n", total_wait);
+	printf("전체고객 방문 수 = %d명 \n", total_customers);
+	printf("전체고객 평균 대기 시간 = %d분 \n", total_wait / total_customers);
+	return 0;
+}
